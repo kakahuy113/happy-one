1544995e498e2c519fede1155aa3a06e02131968
kakahuy113 <kakahuy113@gmail.com>
2020-12-23 17:24:47 +0700

    fix

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -239,13 +239,15 @@ const initFullpage = () => {
 }
 
 $.fn.isInViewport = function() {
-    var elementTop = $(this).offset().top;
+   if(document.querySelector(".index-page")) {
+	var elementTop = $(this).offset().top;
     var elementBottom = elementTop + $(this).outerHeight();
 
     var viewportTop = $(window).scrollTop();
     var viewportBottom = viewportTop + $(window).height();
 
     return elementBottom > viewportTop && elementTop < viewportBottom;
+   }
 };
 
 
